d3c3bd6f82c888f2e025277b4b11ef2e52368aaf
Kurt Zubia <kristhyanlz@gmail.com>
2017-07-21 16:15:31 -0500

    KZ

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -1,10 +1,45 @@
-var celdas = document.getElementsByTagName('td');
+var nombres = [];
+var apellidos = [];
+var dni = [];
+var estado = [];
+var asiento = -1;
+//var celdas = document.getElementsByTagName('td');
 var asientos = [];
-for (var i = 0; i < celdas.length; i++) {
-    celdas[i].addEventListener('click',redirect,false);
+
+function ini(){
+	for(int i = 0; i < 32; ++i){
+		nombres.push(" ");
+		apellidos.push(" ");
+		dni.push(" ");
+	}
 }
-function redirect(event){
-    document.getElementById("mostrar").innerHTML=(event.target.textContent);
+
+function mostrarOcultar(a){
+	if(a == true)
+	document.getElementById("resultado").style.display = "block";
+	else if(a == false)
+	document.getElementById("resultado").style.display = "none";
+	//document.write("gooo");
+}
+
+function cambiarAsiento(a){
+	asiento = a;
+}
+
+function reservar(){
+	//nombres[asiento] = document.getElementById("nombre").text;
+	alert(document.getElementById("nombre").text);
+}
+
+/*for (var i = 0; i < celdas.length; i++) {
+    celdas[i].addEventListener('click',redirect,false);
+}*/
+
+/*
+
+function mostrar(){
+	document.write("<td>Nombres:</td>");
+    //document.getElementById("mostrar").innerHTML=(event.target.textContent);
 }
 
 
@@ -20,3 +55,4 @@ function reservar () {
 
 	 
 }
+*/

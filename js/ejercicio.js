27686d4e0bf48b909650b53d43d4267dc60bff1b
Kurt Zubia <kristhyanlz@gmail.com>
2017-07-21 16:12:40 -0500

    kz

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -2,12 +2,33 @@ var nombres = [];
 var apellidos = [];
 var dni = [];
 var estado = [];
+var asiento = -1;
 //var celdas = document.getElementsByTagName('td');
 var asientos = [];
 
 function ini(){
-	//document.getElementById("resultado").style.display = "none";
-	document.write("gooo");
+	for(int i = 0; i < 32; ++i){
+		nombres.push(" ");
+		apellidos.push(" ");
+		dni.push(" ");
+	}
+}
+
+function mostrarOcultar(a){
+	if(a == true)
+	document.getElementById("resultado").style.display = "block";
+	else if(a == false)
+	document.getElementById("resultado").style.display = "none";
+	//document.write("gooo");
+}
+
+function cambiarAsiento(a){
+	asiento = a;
+}
+
+function reservar(){
+	//nombres[asiento] = document.getElementById("nombre").text;
+	alert(document.getElementById("nombre").text);
 }
 
 /*for (var i = 0; i < celdas.length; i++) {
